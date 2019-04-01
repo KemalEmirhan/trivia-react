512737328d8b74453242ab681bb50d0eaf3120e9
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2019-04-01 14:59:38 +0300

    Added Style
    Modal and Header's style has fixed.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { useState } from "react";
 import styled from "styled-components";
+
+import { Modal, Button } from "antd";
+import "antd/dist/antd.css";
+import "./App.css";
 
 const Wrapper = styled.div`
   text-align: center;
 `;
 
-class App extends Component {
-  render() {
-    return (
-      <Wrapper>
-        <h1>TRIVIA REACT</h1>
-      </Wrapper>
-    );
-  }
-}
+const Header = styled.h1`
+  width: 100%;
+  height: 10rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setVisible(false);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  return (
+    <Wrapper>
+      <Header>TRIVIA GAME</Header>
+      <Button className="btn-start" type="primary" onClick={showModal}>
+        Play Start
+      </Button>
+
+      <Modal
+        title="Trivia Game"
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        centered
+      />
+    </Wrapper>
+  );
+};
 
 export default App;
